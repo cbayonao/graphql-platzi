1f57b4ab0229680d8f7904e863cc9ae89669fd28
cbayonao <drcamilob@live.com>
2020-12-28 00:19:29 -0500

    [Modify] Recibiendo parametros

--- index.js
+++ index.js
@@ -1,30 +1,26 @@
 'use strict'
 
-const { buildSchema } = require('graphql')
+const { makeExecutableSchema } = require('graphql-tools')
 const express = require('express')
     // Esta ya no se usa
     // const gqlMiddleware = require('express-graphql');
 const { graphqlHTTP } = require('express-graphql')
 const { readFileSync } = require('fs')
 const { join } = require('path')
+    // Configurar los resolvers
+    // Importante validar el tipo que se definio en el schema
+const resolvers = require('./lib/resolvers')
 
 const app = express()
 const port = process.env.port || 3000
 
 // Definiendo el shcema de graphql
-const schema = buildSchema(
-    readFileSync(join(__dirname, 'lib', 'schema.graphql'), 'utf-8')
-);
+const typeDefs = readFileSync(join(__dirname, 'lib', 'schema.graphql'), 'utf-8')
+const schema = makeExecutableSchema({ typeDefs, resolvers })
 
-// Configurar los resolvers
-// Importante validar el tipo que se definio en el schema
-const resolvers = {
-        hola: () => {
-            return 'Hola al Mundo'
-        }
-    }
-    // Definimos el middleware
-    // requiere una url y la ejecucion del middleware
+
+// Definimos el middleware
+// requiere una url y la ejecucion del middleware
 app.use('/api', graphqlHTTP({
     schema: schema,
     rootValue: resolvers,
